116a11bee379cc9858401011c45cb1f5af9f8831
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2019-11-13 03:30:02 +0000

    Init

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 const express = require('express')
 const app = express()
 const port = 3000
+// set data source
+const movieList = require('./movies.json')
+// setting template engine from Pug to Handlebars
+const exphbs = require('express-handlebars')
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// setting static files
+app.use(express.static('public')) 
 
 app.get('/', (req, res) => {
-    res.send('Movie List')
+  // past the movie data into 'index' partial template
+  res.render('index', { movies: movieList.results })
+})
+
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const movies = movieList.results.filter(movie => {
+    return movie.title.toLowerCase().includes(keyword.toLowerCase())
+  })
+  res.render('index', { movies: movies, keyword: keyword})
+})
+
+app.get('/movies/:movie_id', (req, res) => {
+  const movie = movieList.results.find
+  (movie => movie.id.toString() === req.params.movie_id)
+  res.render('show', { movie: movie })
 })
 
 app.listen(port, () => {
-    console.log(`App is listening on ${port}`)
+    console.log(`Express is listening on ${port}`)
 })
